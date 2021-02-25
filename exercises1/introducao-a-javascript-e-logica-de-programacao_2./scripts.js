f7d7472fb6d92a3869a0970930c2c7e465a5aa41
Hernanne <hernannedomiciano@gmail.com>
2021-02-25 20:20:39 -0300

    exercicio 2

--- exercises1/introducao-a-javascript-e-logica-de-programacao_2./scripts.js
+++ exercises1/introducao-a-javascript-e-logica-de-programacao_2./scripts.js
@@ -1,31 +1,11 @@
-/*
-let numbers = [5, 9, 3, 19, 70, 8, 10, 2, 2, 1];
+//Exercicio 2
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0;
-let media;
-for (let i of numbers) {
-  soma += i;
+for(let index = 0; index < numbers.length; index++ ){
+    soma =+ numbers[index]
+    console.log(soma);
 }
-media = soma/numbers.length
-console.log(`A média aritmética é: ${media}`)
-if (media > 20) {
-  console.log('maior que 20')
-}
-  else {
-    (media <= 20)
-    console.log ('menor que 20')
-  }
-  */ 
 
-
-
- 
- let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let maiorNum = 0;
-for (let index of numbers) {
-  if (menorNum < index) {
-    maiorNum = index;
-  }
-}
-console.log(maiorNum);
 
 
