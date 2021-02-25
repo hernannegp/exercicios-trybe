d0dfbaf9720b3364e0e0e300f78c80d19df9993d
Hernanne <hernannedomiciano@gmail.com>
2021-02-25 12:17:56 -0300

    exercicios 4.1

---
+++ exercises1/introducao-a-javascript-e-logica-de-programacao_1/scripts.js
@@ -0,0 +1,98 @@
+/*
+let a = 5;
+let b = 8;
+let adicao = a + b;
+let subtracao = a - b;
+let multiplicacao = a * b;
+let divisao = a / b;
+let modulo = a % b;
+
+if (adicao > subtracao) {
+  console.log (adicao);
+}
+  else if (subtracao > adicao) {
+    console.log (subtracao);
+  }
+  */
+
+
+
+
+ /*
+ let n1 = 12;
+ let n2 = 9;
+ let n3 = 6;
+ if (n1 > n2 || n1 > n3){
+     console.log(n1)
+ }else if (n2 > n1 || n2 > n3){
+     console.log(n2)
+ }else if (n3 > n1 || n3 > n2){
+     console.log(n3)
+ }
+ */
+
+
+/*
+let n1 = 90
+let n2 = 90
+
+if (n1 > n2) {
+  console.log ('positive');
+} else if (n2 > n1) {
+  console.log ('negative');
+} else {
+  let n3 = 0
+  console.log (n3)
+}
+*/
+
+
+/*
+let n1 = 60
+let n2 = 60
+let n3 = 60
+
+if (n1 + n2 + n3 == 180) {
+  console.log ('a soma esta correta')
+}
+else {
+  console.log ('a soma esta incorreta')
+}
+*/
+
+/*
+let Cavalo = "movimento em L"
+let Bispo = "Diagonal"
+let Peao = "Duas ou uma casa pra frente"
+let Torre = "Horizontal e Vertical"
+let Dama = "Horizontal Vertical e Diagonal"
+let Rei =  "Uma casa por vez"
+
+*/
+
+let nota = 102
+ 
+if (nota >= 90 && nota <= 100){
+  console.log ('A')
+}
+  else if (nota >= 80 && nota < 90){
+  console.log ('B') 
+  }
+    else if (nota >= 70 && nota < 80){
+      console.log ('C')
+    }
+      else if (nota >= 60 && nota < 70) {
+        console.log ('D')
+      }
+        else if (nota >= 50 && nota < 60) {
+          console.log ('E')
+        }
+          else if (nota < 50 && nota > 0) {
+            console.log ('F')
+          }
+            else {
+              (nota <= 0)
+              console.log ('ERRO')
+           }
+
+       
