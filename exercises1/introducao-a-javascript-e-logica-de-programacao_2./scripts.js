591a705dfaa2c04af3d7f1bd9dcdcbe1343b3c61
Hernanne <hernannedomiciano@gmail.com>
2021-02-25 20:29:12 -0300

    exercicio 7

--- exercises1/introducao-a-javascript-e-logica-de-programacao_2./scripts.js
+++ exercises1/introducao-a-javascript-e-logica-de-programacao_2./scripts.js
@@ -1,15 +1,10 @@
-//Exercicio 6 
+//Exercicio 7 
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let impares = 0;
-
-for (let index of numbers){
-    if (index % 2 == 1){
-        impares++
+let menorValor = numbers[0];
+for(let index of numbers){
+    if (menorValor > index) {
+      menorValor = index;
     }
 }
-if(impares > 0){
-    console.log('numeros impares  :', impares )
-}else{
-    console.log('Não impares')
-}
+console.log(menorValor);
