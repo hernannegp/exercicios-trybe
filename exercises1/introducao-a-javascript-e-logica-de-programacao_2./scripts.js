9affeacc89599ce6a8b44064fe95bcb1537e96dc
Hernanne <hernannedomiciano@gmail.com>
2021-02-25 20:29:59 -0300

    exercicios 8

--- exercises1/introducao-a-javascript-e-logica-de-programacao_2./scripts.js
+++ exercises1/introducao-a-javascript-e-logica-de-programacao_2./scripts.js
@@ -1,10 +1,5 @@
-//Exercicio 7 
+//Exercicio 8
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let menorValor = numbers[0];
-for(let index of numbers){
-    if (menorValor > index) {
-      menorValor = index;
-    }
-}
-console.log(menorValor);
+for (index = 1; index <= 25; index++) {
+  console.log(index);
+} 
