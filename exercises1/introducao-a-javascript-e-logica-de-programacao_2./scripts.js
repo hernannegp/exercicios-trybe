1944fecfa039c6fe4636a517f7129d92e35a9916
Hernanne <hernannedomiciano@gmail.com>
2021-02-25 20:22:25 -0300

    exercicio 3

--- exercises1/introducao-a-javascript-e-logica-de-programacao_2./scripts.js
+++ exercises1/introducao-a-javascript-e-logica-de-programacao_2./scripts.js
@@ -1,11 +1,12 @@
-//Exercicio 2
+//Exercicio 3
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0;
-for(let index = 0; index < numbers.length; index++ ){
-    soma =+ numbers[index]
-    console.log(soma);
+let media;
+for (index of numbers) {
+  soma += index;
 }
-
+media = soma/numbers.length;
+console.log(media);
 
 
