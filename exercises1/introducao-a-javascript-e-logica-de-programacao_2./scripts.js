cccf2d2a5294212fb8579c0afd6bbb7cc6b826b1
Hernanne <hernannedomiciano@gmail.com>
2021-02-25 20:24:32 -0300

    exercicio 4

--- exercises1/introducao-a-javascript-e-logica-de-programacao_2./scripts.js
+++ exercises1/introducao-a-javascript-e-logica-de-programacao_2./scripts.js
@@ -1,4 +1,5 @@
-//Exercicio 3
+
+//Exercicio 4
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0;
@@ -6,7 +7,10 @@ let media;
 for (index of numbers) {
   soma += index;
 }
-media = soma/numbers.length;
-console.log(media);
-
-
+media = soma/numbers.length
+console.log(media)
+if (media > 20){
+    console.log('A media é maior que 20');
+}else{
+    console.log('A media é menor que 20');
+}
