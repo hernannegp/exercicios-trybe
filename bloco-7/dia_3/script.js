0976a5b143094578d44eed85e617625728cb7f15
Hernanne <hernannedomiciano@gmail.com>
2021-03-25 20:49:45 -0300

    exercises pt 1

--- bloco-7/dia_3/script.js
+++ bloco-7/dia_3/script.js
@@ -32,3 +32,59 @@
 // myRemove(myList, 5)
 // assert.deepStrictEqual(myList, [5, 7, 9, 10, 11])
 // exercicio 3
+// const assert = require('assert');
+
+// function myRemoveWithoutCopy(arr, item) {
+//   for (let index = 0, len = arr.length; index < len; index += 1) {
+//     if (arr[index] === item) {
+//       arr.splice(index, 1);
+//       index -= 1;
+//       len -= 1;
+//     }
+//   }
+
+//   return arr;
+// }
+// assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2, 4])
+// // assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2, 3, 4])
+// assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 5), [1, 2, 3, 4])
+
+//  const myList = [5, 7, 9, 10, 11]
+//  myRemoveWithoutCopy(myList, 5)
+//  assert.deepStrictEqual(myList, [7, 9, 10, 11])
+// exercicio 4
+// const assert = require('assert');
+
+// function myFizzBuzz(num) {
+//   if (typeof num !== 'number') return false;
+//   if (num % 3 === 0 && num % 5 === 0) return 'fizzbuzz';
+//   if (num % 3 === 0) return 'fizz';
+//   if (num % 5 === 0) return 'buzz';
+//   return num;
+// }
+// assert.strictEqual(myFizzBuzz(15), "fizzbuzz")
+// assert.strictEqual(myFizzBuzz(9), "fizz")
+// assert.strictEqual(myFizzBuzz(20), "buzz")
+// assert.strictEqual(myFizzBuzz(4), 4)
+// assert.strictEqual(myFizzBuzz("4"), false)
+// exercicio 5
+// const assert = require('assert');
+
+// const obj1 = {
+// title: 'My Title',
+// description: 'My Description',
+// };
+
+// const obj2 = {
+// description: 'My Description',
+// title: 'My Title',
+// };
+
+// const obj3 = {
+// title: 'My Different Title',
+// description: 'My Description',
+// };
+// assert.deepStrictEqual(obj1, obj2)
+// assert.notDeepStrictEqual(obj1, obj3)
+// assert.notDeepStrictEqual(obj2, obj3)
+// parte 2
